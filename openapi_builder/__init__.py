a50480b81a6d0e7bb4be2a175e8958129f587d9b
Patrick Vogel <patrick.vogel@seetickets.nl>
2022-02-06 20:34:17 +0100

    Added how-it-works section to README

--- openapi_builder/__init__.py
+++ openapi_builder/__init__.py
@@ -1,12 +1,10 @@
 from . import __meta__
 from .builder import DocumentationOptions, OpenApiDocumentation
-from .documentation import DiscriminatorOptions
 from .decorators import add_documentation, set_schema_options, set_resource_options
 
 __version__ = __meta__.version
 
 __all__ = [
-    "DiscriminatorOptions",
     "DocumentationOptions",
     "OpenApiDocumentation",
     "add_documentation",
