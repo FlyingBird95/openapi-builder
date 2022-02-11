5559bd7e6fd3769e17c212740eee928cc275bb0f
Patrick Vogel <patrick.vogel@seetickets.nl>
2022-02-11 22:30:15 +0100

    Improved documentation

--- docs/conf.py
+++ docs/conf.py
@@ -34,125 +34,59 @@ now = datetime.datetime.now()
 project = meta.name
 project_path = meta.path
 author = meta.author
-copyright = "{}, {}".format(now.year, author)
+copyright = f"{now.year}, {author}"
 
 
 # The full version, including alpha/beta/rc tags
 release = meta.version
-# The short X.Y version
-version = ".".join(release.split(".")[0:2])
+# The short X.Y.Z version
+version = ".".join(release.split(".")[0:3])
 
 
-# -- Automatically generate API documentation --------------------------------
-
-
-def run_apidoc(_):
-    """
-    Call apidoc, with customised set up.
-    """
-    ignore_paths = [
-        os.path.join("..", project_path, "tests"),
-    ]
-
-    argv = [
-        "--force",  # Overwrite output files
-        "--follow-links",  # Follow symbolic links
-        "--separate",  # Put each module file in its own page
-        "--module-first",  # Put module documentation before submodule
-        "-o",
-        "source/packages",  # Output path
-        os.path.join("..", project_path),
-    ] + ignore_paths
-
-    try:
-        # Sphinx 1.7+
-        from sphinx.ext import apidoc
-
-        apidoc.main(argv)
-    except ImportError:
-        # Sphinx 1.6 (and earlier)
-        from sphinx import apidoc
-
-        argv.insert(0, apidoc.__file__)
-        apidoc.main(argv)
-
-
-def retitle_modules(_):
-    """
-    Overwrite the title of the modules.rst file.
-    """
-    pth = "source/packages/modules.rst"
-    lines = open(pth).read().splitlines()
-    # Overwrite the junk in the first two lines with a better title
-    lines[0] = "API Reference"
-    lines[1] = "============="
-    open(pth, "w").write("\n".join(lines))
-
-
-def auto_convert_readme(_):
-    """
-    Handle README.rst or README.md as available.
-
-    If it exists, makes a symbolic link to README.rst at docs/source/readme.rst.
-    Otherwise, and if it exists, converts README.md to to rST format, the
-    output of which is docs/source/readme.rst.
-    """
-    readme_path_md = os.path.join(REPO_DIRECTORY, "README.md")
-    readme_path_rst = os.path.splitext(readme_path_md)[0] + ".rst"
-    readme_path_output = os.path.join(DOCS_DIRECTORY, "source", "readme.rst")
-    # Ensure output directory exists
-    output_dir = os.path.dirname(readme_path_output)
+def copy_file(file_from, file_to):
+    output_dir = os.path.dirname(file_to)
     os.makedirs(output_dir, exist_ok=True)
 
-    if os.path.isfile(readme_path_rst):
+    if os.path.isfile(file_from):
         # Make docs/source/readme.rst be a symbolic link to README.rst
         #
         # We can't overwrite an existing file when calling os.symlink, so
         # we write to a temporary file and copy that over instead.
         tmp_path = tempfile.mktemp(dir=output_dir)
         try:
-            os.symlink(readme_path_rst, tmp_path)
-            os.replace(tmp_path, readme_path_output)
+            os.symlink(file_from, tmp_path)
+            os.replace(tmp_path, file_to)
         finally:
             if os.path.islink(tmp_path):
                 os.remove(tmp_path)
 
-    elif os.path.isfile(readme_path_md):
-        # Otherwise, if README.md exists convert it to markdown using pandoc
-        import pypandoc
 
-        # Download pandoc if necessary. If pandoc is already installed and on
-        # the PATH, the installed version will be used. Otherwise, we will
-        # download a copy of pandoc into docs/bin/ and add that to our PATH.
-        pandoc_dir = os.path.join(DOCS_DIRECTORY, "bin")
-        os.environ["PATH"] += os.pathsep + pandoc_dir
-        pypandoc.ensure_pandoc_installed(
-            quiet=True,
-            targetfolder=pandoc_dir,
-            delete_installer=True,
-        )
-        # Call pandoc using the pypandoc wrapper
-        tmp_path = tempfile.mktemp(dir=output_dir)
-        try:
-            pypandoc.convert_file(
-                readme_path_md,  # Source file
-                "rst",  # to rST
-                format="gfm",  # from GitHub-Flavored Markdown
-                outputfile=tmp_path,  # Output file
-            )
-            os.replace(tmp_path, readme_path_output)
-        finally:
-            if os.path.islink(tmp_path):
-                os.remove(tmp_path)
+def copy_files(_):
+    """Makes a symbolic link to README.rst at docs/source/readme.rst."""
+    readme_path = os.path.join(REPO_DIRECTORY, "README.rst")
+    readme_path_output = os.path.join(DOCS_DIRECTORY, "source", "readme.rst")
+    copy_file(readme_path, readme_path_output)
+
+    # copy images
+    img_dir = os.path.join(REPO_DIRECTORY, "img")
+    img_dir_output = os.path.join(DOCS_DIRECTORY, "source", "img")
+    img2_dir_output = os.path.join(DOCS_DIRECTORY, "img")
+    for filename in os.listdir(img_dir):
+        img_file = os.path.join(img_dir, filename)
+        if not os.path.isfile(img_file):
+            continue  # skip directories
+        copy_file(img_file, os.path.join(img_dir_output, filename))
+        copy_file(img_file, os.path.join(img2_dir_output, filename))
+
+    # copy changelog
+    changelog_path = os.path.join(REPO_DIRECTORY, "CHANGELOG.rst")
+    changelog_path_output = os.path.join(DOCS_DIRECTORY, "source", "changelog.rst")
+    copy_file(changelog_path, changelog_path_output)
 
 
 def setup(app):
-    """
-    Set up our apidoc commands to run whenever sphinx is built.
-    """
-    app.connect("builder-inited", auto_convert_readme)
-    app.connect("builder-inited", run_apidoc)
-    app.connect("builder-inited", retitle_modules)
+    """Set up our apidoc commands to run whenever sphinx is built."""
+    app.connect("builder-inited", copy_files)
 
 
 # -- General configuration ---------------------------------------------------
@@ -165,35 +99,8 @@ def setup(app):
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
-    "myst_parser",  # Support markdown
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosectionlabel",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
-    "sphinx.ext.coverage",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.ifconfig",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
     "sphinx_search.extension",
 ]
-
-# Napoleon settings
-napoleon_google_docstring = True
-napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = True
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = False
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_rtype = True
-napoleon_use_param = True
-napoleon_type_aliases = {
-    "array-like": ":term:`array-like <array_like>`",
-    "array_like": ":term:`array_like`",
-}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -201,7 +108,7 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = [".rst", ".md"]
+source_suffix = [".rst"]
 
 # The master toctree document.
 master_doc = "index"
@@ -238,7 +145,7 @@ html_theme = "sphinx_book_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -331,19 +238,14 @@ epub_exclude_files = ["search.html"]
 # Example configuration for intersphinx
 # Common intersphinx mappings can be found here:
 # https://gist.github.com/bskinn/0e164963428d4b51017cebdb6cda5209
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/{.major}".format(sys.version_info), None),
-    "attrs": ("https://www.attrs.org/en/stable/", None),
-    "numpy": ("https://numpy.org/doc/stable/", None),
-    "scipy": ("https://docs.scipy.org/doc/scipy/reference/", None),
-    "matplotlib": ("https://matplotlib.org/stable/", None),
-    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
-    "Pillow": ("https://pillow.readthedocs.io/en/stable/", None),
-    "skimage": ("https://scikit-image.org/docs/stable/", None),
-    "sklearn": ("https://scikit-learn.org/stable/", None),
-}
-
-# -- Options for todo extension ----------------------------------------------
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
+# intersphinx_mapping = {
+#     "python": ("https://docs.python.org/{.major}".format(sys.version_info), None),
+#     "attrs": ("https://www.attrs.org/en/stable/", None),
+#     "numpy": ("https://numpy.org/doc/stable/", None),
+#     "scipy": ("https://docs.scipy.org/doc/scipy/reference/", None),
+#     "matplotlib": ("https://matplotlib.org/stable/", None),
+#     "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+#     "Pillow": ("https://pillow.readthedocs.io/en/stable/", None),
+#     "skimage": ("https://scikit-image.org/docs/stable/", None),
+#     "sklearn": ("https://scikit-learn.org/stable/", None),
+# }
